0fe71f4a7a506dbd9564ac69d3907f7f40b3b225
Caleb Danielsen <caleb@lfpost.dk>
2021-08-13 17:32:44 +0200

    start of project

--- Notes/Controller/FolderViewController.swift
+++ Notes/Controller/FolderViewController.swift
@@ -4,15 +4,20 @@
 //
 //  Created by Caleb Danielsen on 13/08/2021.
 //
+// Task:
+// 1. add a component to the tableView
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FolderViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    
 
 
 }
