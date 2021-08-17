09b154bfaacd1fc8c8978f6bc8fd5be7474986c6
Caleb Danielsen <caleb@lfpost.dk>
2021-08-17 15:25:46 +0200

    small change

--- Notes/Controller/TextViewController.swift
+++ Notes/Controller/TextViewController.swift
@@ -15,21 +15,8 @@ class TextViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        text.delegate = self
-        
-        text.textColor = .placeholderText
         text.becomeFirstResponder()
     }
     
-    
-}
-
-extension TextViewController: UITextViewDelegate {
-    
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        text.text = ""
-        
-    }
     
 }
